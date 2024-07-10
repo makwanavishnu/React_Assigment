87d125456bcd524301d03de43d86ecd6471e7a6d
Vishnu Makwana <68947446+makwanavishnu@users.noreply.github.com>
2024-07-10 14:12:04 +0530

    react assigment

---
+++ my-new-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p style={{backgroundColor:'#61dafb',height:"100px" ,width :"100%", paddingTop:"50px" ,fontSize:"50px"}}>
+           Hellow World
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         My Frist Project
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-new-app/src/FirstComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ function FirstComponent() {
+  return (
+    <div>FirstComponent</div>
+  )
+}
+export default FirstComponent
